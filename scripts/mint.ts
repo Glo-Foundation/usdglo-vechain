40524755ab42e2a5bdad7754b7fdfd406294ba27
gglucass <garm.lucassen@gmail.com>
2025-03-06 20:18:13 +0100

    Mint

---
+++ scripts/mint.ts
@@ -0,0 +1,42 @@
+import * as dotenv from "dotenv";
+import * as hre from "hardhat";
+import * as UUPSUpgradeable from '@openzeppelin/contracts/build/contracts/UUPSUpgradeable.json';
+
+async function main() {
+  const currentContractName = process.env.UPGRADE_CONTRACT_NAME as string;
+  const proxyAddress = process.env.PROXY_ADDRESS as string;
+  const initialAdminAddress = process.env.INITIAL_ADMIN_ADDRESS as string;
+
+  const Token = await hre.ethers.getContractFactory("GloDollarV3");
+  const token = await Token.attach(proxyAddress);
+
+  const recipient = "0x3B53F4eAC117268Cf77d4ecD602fEF880FEDeAD9";
+  const amountToMint = 10n * (10n ** 18n);
+
+  const recipientBalance = await token.balanceOf(recipient);
+
+  console.log("recipient current balance: ", recipientBalance);
+
+  const mintToAddress = await token.mint(recipient, amountToMint);
+
+  const recipientNewBalance = await token.balanceOf(recipient);
+
+  console.log("recipient new balance: ", recipientNewBalance);
+
+  // const proxy = await hre.ethers.getContractAt(UUPSUpgradeable.abi, proxyAddress)
+  // console.log(proxy);
+
+}
+
+if (require.main === module) {
+  dotenv.config();
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
+
+
+
